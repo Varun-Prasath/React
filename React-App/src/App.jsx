c17b1be5733db5623d9de32f1c24edd76e1097f2
Varun Prasath K <varunkprasath@gmail.com>
2025-11-17 17:16:53 +0530

    Utilised the cars component to create a webpage displaying cars

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -2,34 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Welcome from './Welcome'
+import Cars from './Cars'
+import Toyota from './assets/hyryder.jpeg'
+import Venue from './assets/venue.jpg'
+import Virtus from './assets/virtus.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Cars name="Toyota" image={Toyota} desc="This is the Toyota HyRyder"></Cars>
+    <Cars name="Hyundai" image={Venue} desc="This is the Hyundai Venue"></Cars>
+    <Cars name="Volkswagen" image={Virtus} desc="This is the Volkswagen Virtus"></Cars>
     </>
-  )
+  );
 }
 
 export default App
