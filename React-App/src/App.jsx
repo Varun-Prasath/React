ab87196866addd9deb024dd34e3215cd0aa864b7
Varun Prasath K <varunkprasath@gmail.com>
2025-11-20 14:15:20 +0530

    adding extra form fields and stlyes

--- React-App/src/App.jsx
+++ React-App/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import Welcome from './Welcome'
 import Counter from './Counter'
 import CarsList from './CarsList'
--- React-App/src/Form.jsx
+++ React-App/src/Form.jsx
@@ -1,24 +1,42 @@
 import { useState } from "react";
+import './form.css'
 
 function Form(){
+    let [name, setName] = useState("");
     let [pass, setPass] = useState("");
+    let [mail, setMail] = useState("");
+    let [age, setAge] = useState(0);
 
     function handlePass(event){
         setPass(event.target.value)
     }
 
+    function handleName(event){
+        setName(event.target.value);
+    }
+
     return(
         <>
-            <form>
+            <form onSubmit={(e)=>e.preventDefault()}>
                 <label htmlFor="Name">Name </label>
-                <input type="text" /><br /><br />
+                <input type="text" value={name} onChange={handleName} />
+                <p>{name}</p><br />
                 <label htmlFor="mail">Email </label>
-                <input type="email" /><br /><br />
+                <input type="email" value={mail} onChange={(e)=> setMail(e.target.value)}/>
+                <p>Characters: {mail.length}</p><br />
+                <label htmlFor="Age">Age </label>
+                <input type="number" value={age} onChange={(e) => setAge(e.target.value)}/><br /><br />
                 <label htmlFor="pass">Password </label>
                 <input type="password" value={pass} onChange={handlePass}/>
-                <p>{pass.length < 3 ? "Password should be more than 3 chars" : ""}</p>
-                <button>Submit</button>
+                <p>{pass.length <= 3 ? "Password should be more than 3 chars" : ""}</p>
+                <button disabled={mail.length == 0}>Submit</button>
             </form>
+            <div className="card">
+                <h3>Preview</h3>
+                <p><b>Name:</b> {name || "—"}</p>
+                <p><b>Email:</b> {mail || "—"}</p>
+                <p><b>Age:</b> {age || "—"}</p>
+            </div>
         </>
     );
 }
